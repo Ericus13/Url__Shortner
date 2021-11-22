a842756401fe46931ef6ac906e484f48b26c32b4
kenselasie <kenselasie@gmail.com>
2021-11-22 11:30:23 +0000

    Fix: url shortener

--- shortner/views.py
+++ shortner/views.py
@@ -4,6 +4,7 @@ from uuid import uuid4
 from .models import Url
 # Create your views here.
 
+urlLists = []
 def index(request):
     return render(request, "interface.html")
 
@@ -12,9 +13,20 @@ def create(request):
         l = request.POST["link"]
         uid = str(uuid4())[:5]
         new_url = Url(uuid =uid)
-        new_url.save
+        new_url.save()
+        urlLists.append({
+            'key': uid,
+            'value': l
+        })
         return HttpResponse(uid)
 
 def go(request, pk):
-    url_details = Url.objects.get(uuid=pk)
-    return redirect('https://' + url_details.link)
+    # url_details = Url.objects.get(uuid=pk)
+    print(pk)
+    url = ''
+    for i in urlLists:
+        if (i['key'] == pk):
+            url = i['value']
+
+    print(url)
+    return redirect(url)
